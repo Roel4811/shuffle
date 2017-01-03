0b36a7028208c505e5c36e648554a63263f9afa4
Roel de Jong <Roel4811@hotmail.com>
2017-01-03 19:17:20 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :guests
+  root 'guests#index'
   # # show all guests
   # get "guests", to: "guests#index"
 
