696be901f26874f9b3f28891e44885fa84fb5fe7
Roel de Jong <Roel4811@hotmail.com>
2017-01-24 15:38:42 +0100

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Guest.destroy_all
-Guest.create(name: "Felix de Rijk", age: 24, location: "Breda")
-Guest.create(name: "Random chick", age: 28, location: "Amsterdam")
+Guest.create(name: "Felix de Rijk", age: 24, location: "Breda", description: "Ik ben een lekkere jongen uit breda op zoek naar actie")
+Guest.create(name: "Random chick", age: 28, location: "Amsterdam", description: "Na een gebroken hart ben ik weer klaar voor de datingwereld")
+Guest.create(name: "Roel de Jong", age: 24, location: "Breda", description: "gezellige boy uit het zuiden van het land")
+Guest.create(name: "Jake the Dog", age: 27, location: "adventureland", description: "ik ben een magische hond die kan praten")
+Guest.create(name: "Thijmen", age: 24, location: "Rotterdam", description: "een goedlachse jongeman van alle markten thuis")
