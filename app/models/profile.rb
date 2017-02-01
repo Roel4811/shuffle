548f3d7f02181233d186ab4d8f444ea2dfaf8f4f
Roel de Jong <Roel4811@hotmail.com>
2017-02-01 14:37:11 +0100

    add associations to user and profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ApplicationRecord
+  belongs_to :user
   mount_uploader :photo, PhotoUploader
   validates :name, :age, :location, presence: true
   validates :age, :numericality => { greater_than_or_equal_to: 18  }
