86a9df6642d78aaecb65a9d4aac9ada5b2073744
Roel de Jong <Roel4811@hotmail.com>
2017-02-01 13:27:13 +0100

    skip authentication bij index

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
 
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, :only: [:index]
 
   def index
     @profiles = Profile.all
