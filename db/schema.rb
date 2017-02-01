d09c05720cc84aaded868f3091dcc855a5a5a7c1
Roel de Jong <Roel4811@hotmail.com>
2017-02-01 14:32:33 +0100

    migrate profile reference to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130163408) do
+ActiveRecord::Schema.define(version: 20170201132948) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
@@ -36,7 +36,9 @@ ActiveRecord::Schema.define(version: 20170130163408) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "profile_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["profile_id"], name: "index_users_on_profile_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
