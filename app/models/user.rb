002939a514e94f398d1953a80821b80c20823451
Roel de Jong <Roel4811@hotmail.com>
2017-02-10 15:06:42 +0100

    update header links and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :email, presence: true
+
+  attr_accessor :login
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  # as :user do
+  #   get 'login', to: 'devise/sessions#new'
+  # end
+
   resources :profiles
   root 'profiles#index'
   # # show all profiles
