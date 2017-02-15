cee455c06520a544b6b25832251eb0453614d570
Roel de Jong <Roel4811@hotmail.com>
2017-02-15 18:27:12 +0100

    add birthdate to user model and homeview

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209165007) do
+ActiveRecord::Schema.define(version: 20170215170918) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170209165007) do
     t.datetime "updated_at",                          null: false
     t.integer  "profile_id"
     t.string   "username"
+    t.date     "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["profile_id"], name: "index_users_on_profile_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
