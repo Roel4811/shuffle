d16946356dbff320f66ea510faf0b4c0a350053c
Roel de Jong <Roel4811@hotmail.com>
2017-02-16 18:42:21 +0100

    update routes and js for redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount ActionCable.server => '/cable'
   devise_for :users
+  devise_for :admins
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # as :user do
