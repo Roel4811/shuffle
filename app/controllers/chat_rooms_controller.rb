4e33d121847c17f319dcc6c9a2741a26f96dd9ca
Roel de Jong <Roel4811@hotmail.com>
2017-02-16 18:48:17 +0100

    add message instance variable to chat_rooms#show

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -5,6 +5,7 @@ class ChatRoomsController < ApplicationController
 
   def show
     @chat_room = Chatroom.includes(:messsages).find_by(id: params[:id])
+    @message = Message.new
   end
 
   def new
