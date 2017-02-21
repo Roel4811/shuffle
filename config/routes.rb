4a5d996fce07f4f48893f96808cab9bd36ad40ed
Roel de Jong <Roel4811@hotmail.com>
2017-02-21 17:48:13 +0100

    update devise for facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount ActionCable.server => '/cable'
-  devise_for :users
+
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   devise_for :admins
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
